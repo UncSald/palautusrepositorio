b38ee07a1aa416f594096786ea3a514a9e25b3e3
UncSald <iikka.alingue@gmail.com>
2024-12-20 19:44:10 +0200

    Exercises 3-5 for week 7.

---
+++ viikko7/MERGE.md
@@ -0,0 +1,1 @@
+[Mergetty pull request](https://github.com/UncSald/KettuRepolainen/pull/1)
---
+++ viikko7/kivi-paperi-sakset/src/kps_pelaaja_vs_pelaaja.py
@@ -0,0 +1,8 @@
+from kps_ylaluokka import KiviPaperiSakset
+
+
+class KPSPelaajaVsPelaaja(KiviPaperiSakset):
+    def _toisen_siirto(self, ensimmaisen_siirto):
+        tokan_siirto = input("Toisen pelaajan siirto: ")
+
+        return tokan_siirto
